3e141f764e747b6355bac0f63e0237843d416408
Alexander Chernokun <alexchernokun@icloud.com>
2023-04-05 12:46:32 +0300

    Initial commit

---
+++ HomeRadio/HomeRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeRadioApp.swift
+//  HomeRadio
+//
+//  Created by Alexander Chernokun on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct HomeRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
