4a410edd25217239851cbfd2a993b000d50837d6
Alexander Chernokun <alexchernokun@icloud.com>
2023-07-05 21:17:18 +0300

    Minor changes

--- HomeRadio/SearchStationTab/Sub Category/SubCategoryModuleBuilder.swift
+++ HomeRadio/SearchStationTab/Sub Category/SubCategoryModuleBuilder.swift
@@ -31,7 +31,6 @@ struct SubCategoryModuleBuilder: ModuleBuilder {
         let view = SubCategoryView(interactor: interactor,
                                    viewModel: presenter.viewModel)
         return view
-
     }
     
 }
